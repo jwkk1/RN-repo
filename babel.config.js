5ff719524ff2271f206942f38506dfcf21eb7a0d
jwkk1 <dog107.2@gmail.com>
2023-10-27 17:32:26 +0900

    ✨ env 세팅

--- babel.config.js
+++ babel.config.js
@@ -27,5 +27,17 @@ module.exports = {
         },
       },
     ],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+    'react-native-reanimated/plugin',
   ],
 };
