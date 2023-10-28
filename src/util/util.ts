b3f6efb290a6fbf6f4eeddf2092d17c0f64f2f4b
jwkk1 <dog107.2@gmail.com>
2023-10-28 17:15:39 +0900

    ✨ device Info 조회 공통유틸 추가

---
+++ src/util/util.ts
@@ -0,0 +1,11 @@
+import { SERVICE_KEY } from '@env';
+import { Platform } from 'react-native';
+
+export const getDeviceInfo = () => {
+  const phoneInfo = {
+    serviceKey: SERVICE_KEY,
+    MobileOS: Platform.OS === 'ios' ? 'IOS' : 'AND',
+    MobileApp: 'RN-REPO',
+  };
+  return phoneInfo;
+};
