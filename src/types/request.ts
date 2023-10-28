0561f77571f64434cb32c124d2ad8e8f0691c230
jwkk1 <dog107.2@gmail.com>
2023-10-28 21:46:38 +0900

    🏷️ response / request Type 추가

--- src/types/request.ts
+++ src/types/request.ts
@@ -2,7 +2,9 @@ export interface searchKeywordRequest {
   MobileOS: string;
   MobileApp: string;
   keyword: string;
+  _type: string;
   serviceKey: string;
   arrange?: string;
   areaCode?: string;
+  numOfRows?: number;
 }
