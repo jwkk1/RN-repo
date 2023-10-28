c73fd58b646943240f05b1e5a9657d7d1b2760ff
jwkk1 <dog107.2@gmail.com>
2023-10-28 21:45:41 +0900

    ✨ searchKeyword react-query 로직 추가

--- src/pages/Home/_queries.ts
+++ src/pages/Home/_queries.ts
@@ -1,5 +1,6 @@
 import { requestSearchKeyword } from '@/api/getData';
 import { getDeviceInfo } from '@/util/util';
+import Toast from 'react-native-toast-message';
 import { useQuery } from 'react-query';
 
 export const useGetSearchKeyword = (keyword: string) => {
@@ -9,10 +10,30 @@ export const useGetSearchKeyword = (keyword: string) => {
     MobileOS: deviceInfo.MobileOS,
     MobileApp: deviceInfo.MobileApp,
     keyword: keyword,
+    _type: deviceInfo._type,
+    numOfRows: 5,
+    arrange: 'R',
   };
-  const { data, isLoading, refetch } = useQuery(['searchKeyword', keyword], () =>
-    requestSearchKeyword(params),
+  const { data, isLoading, refetch } = useQuery(
+    ['searchKeyword', keyword],
+    () => requestSearchKeyword(params),
+    {
+      onSuccess: async ({ data }) => {
+        if (data.response.header.resultCode !== '0000') {
+          Toast.show({
+            type: 'customToast',
+            text1: data.response.resultMsg,
+          });
+        }
+      },
+      onError: async () => {
+        Toast.show({
+          type: 'customToast',
+          text1: '인터넷 연결을 확인해 주세요.',
+        });
+      },
+    },
   );
 
-  return { searchKeywordList: data, isLoading, refetch };
+  return { searchKeywordList: data?.data.response.body.items.item, isLoading, refetch };
 };
