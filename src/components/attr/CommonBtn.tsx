754596c78d78a7d01cf60359b5c83c74c65fa675
jwkk1 <dog107.2@gmail.com>
2023-10-28 16:20:56 +0900

    💄 commonBtn 추가

---
+++ src/components/attr/CommonBtn.tsx
@@ -0,0 +1,97 @@
+import { colors, fontPercentage, heightPercentage, widthPercentage } from '@/styles/globalStyle';
+import React from 'react';
+import { StyleSheet, Text, Pressable } from 'react-native';
+interface props {
+  buttonStyle: 'solid' | 'solidUnabled' | 'outLine' | 'outLineUnabled' | 'outLine2' | 'text';
+  text: string;
+  handler?: () => void;
+  mr?: boolean;
+}
+
+const CommonButtonMedium = ({ buttonStyle, text, handler, mr }: props) => {
+  const containerStyle = {
+    solid: {
+      backgroundColor: colors.signature,
+      borderColor: colors.signature,
+    },
+    solidUnabled: {
+      backgroundColor: '#f8c3a7',
+      borderColor: '#f8c3a7',
+    },
+    outLine: {
+      backgroundColor: '#FFFFFF',
+      borderColor: colors.signature,
+    },
+    outLineUnabled: {
+      backgroundColor: '#FFFFFF',
+      borderColor: '#f8c3a7',
+    },
+    outLine2: {
+      backgroundColor: '#FFFFFF',
+      borderColor: '#CCCCCC',
+    },
+    text: {
+      backgroundColor: 'transparent',
+      borderColor: 'transparent',
+    },
+  };
+
+  const textStyle = {
+    solid: {
+      color: '#FFFFFF',
+    },
+    solidUnabled: {
+      color: '#FFFFFF',
+    },
+    outLine: {
+      color: colors.signature,
+    },
+    outLineUnabled: {
+      color: '#f8c3a7',
+    },
+    outLine2: {
+      color: '#ABABAB',
+    },
+    text: {
+      color: colors.signature,
+    },
+  };
+
+  return (
+    <Pressable
+      style={[
+        styles.container,
+        containerStyle[buttonStyle],
+        mr && { marginRight: heightPercentage(10) },
+      ]}
+      onPress={handler}
+    >
+      <Text style={[styles.text, textStyle[buttonStyle]]}>{text}</Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 999,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: colors.signature,
+    paddingHorizontal: widthPercentage(15),
+    paddingVertical: heightPercentage(6),
+    backgroundColor: colors.signature,
+  },
+  text: {
+    fontSize: fontPercentage(14),
+    letterSpacing: -0.4,
+    lineHeight: 24,
+    fontWeight: '500',
+    color: '#fff',
+    textAlign: 'left',
+  },
+});
+
+export default CommonButtonMedium;
