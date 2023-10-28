609336a26815d2a4be786e41113b2160c87a2f10
jwkk1 <dog107.2@gmail.com>
2023-10-28 15:45:33 +0900

    ✨ globalSyle 추가

---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,38 @@
+import { StyleSheet } from 'react-native';
+import {
+  responsiveFontSize,
+  responsiveHeight,
+  responsiveWidth,
+} from 'react-native-responsive-dimensions';
+
+const FIGMA__WINDOW__WIDTH = 375;
+const FIGMA__WINDOW__HEIGHT = 812;
+
+export const widthPercentage = (width: number) => {
+  const percentage = (width / FIGMA__WINDOW__WIDTH) * 100;
+
+  return responsiveWidth(percentage);
+};
+export const heightPercentage = (height: number) => {
+  const percentage = (height / FIGMA__WINDOW__HEIGHT) * 100;
+
+  return responsiveHeight(percentage);
+};
+export const fontPercentage = (size: number) => {
+  const percentage = size * 0.125;
+
+  return responsiveFontSize(percentage);
+};
+
+export const globalStyles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  mainContainer: {
+    flex: 1,
+    width: '100%',
+    paddingTop: heightPercentage(20),
+    paddingHorizontal: widthPercentage(20),
+  },
+});
