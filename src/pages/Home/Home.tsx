e34e7c2f2511cef7fdbc1e78363ca5574791f8d7
jwkk1 <dog107.2@gmail.com>
2023-10-29 17:45:25 +0900

    💄 메인페이지 View -> scrollView로 변경

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,17 @@
 import CommonButtonMedium from '@/components/attr/CommonBtn';
+import PetTourArea from '@/components/home/PetTourArea';
 import SearchCategory from '@/components/home/SearchCategory';
 import { globalStyles } from '@/styles/globalStyle';
 import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
 
 const Home = () => {
   return (
     <SafeAreaView style={globalStyles.screen}>
-      <View style={globalStyles.mainContainer}>
+      <ScrollView style={globalStyles.mainContainer} showsVerticalScrollIndicator={false}>
         <SearchCategory />
-      </View>
+        <PetTourArea />
+      </ScrollView>
     </SafeAreaView>
   );
 };
