1f623c4522f0a849e40a87d9d01fe811a3ccda52
jinwooKim-sitin <jinwoo@sitin.co.kr>
2023-10-30 17:51:46 +0900

    ✨ route props 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,17 @@
-import CommonButtonMedium from '@/components/attr/CommonBtn';
 import PetTourArea from '@/components/home/petTour/PetTourArea';
 import SearchCategory from '@/components/home/tour/SearchCategory';
 import { globalStyles } from '@/styles/globalStyle';
-import { HomeNavigatorProps, TourDetailNavigatorProps } from '@/types/navigation';
+import { HomeNavigatorProps } from '@/types/navigation';
 import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 
 type props = HomeNavigatorProps;
 
-const Home = ({ navigation }: props) => {
+const Home = ({ navigation, route }: props) => {
   return (
     <SafeAreaView style={globalStyles.screen}>
       <ScrollView style={globalStyles.mainContainer} showsVerticalScrollIndicator={false}>
-        <SearchCategory navigation={navigation} />
+        <SearchCategory navigation={navigation} route={route} />
         <PetTourArea />
       </ScrollView>
     </SafeAreaView>
