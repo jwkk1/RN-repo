bae2ac24a89b455ad24196feb0ac562ce4fa15f0
jinwooKim-sitin <jinwoo@sitin.co.kr>
2023-10-30 15:42:51 +0900

    🚚 디렉토리 변경으로 import 재설정

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import CommonButtonMedium from '@/components/attr/CommonBtn';
-import PetTourArea from '@/components/home/PetTourArea';
-import SearchCategory from '@/components/home/SearchCategory';
+import PetTourArea from '@/components/home/petTour/PetTourArea';
+import SearchCategory from '@/components/home/tour/SearchCategory';
 import { globalStyles } from '@/styles/globalStyle';
 import React from 'react';
 import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
