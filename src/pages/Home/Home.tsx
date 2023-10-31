5f61870007ebe2bb9c15edc16ce9d043e2e16a48
jwkk1 <dog107.2@gmail.com>
2023-10-31 15:44:28 +0900

    ✨ nav headerright 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,29 @@
 import PetTourArea from '@/components/home/petTour/PetTourArea';
 import SearchCategory from '@/components/home/tour/SearchCategory';
-import { globalStyles } from '@/styles/globalStyle';
+import { globalStyles, widthPercentage } from '@/styles/globalStyle';
 import { HomeNavigatorProps } from '@/types/navigation';
-import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { Pressable, SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+import SearchIcon from '@assets/tabBar/search.svg';
 
 type props = HomeNavigatorProps;
 
 const Home = ({ navigation, route }: props) => {
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <Pressable
+            style={{ marginRight: widthPercentage(20) }}
+            onPress={() => navigation.navigate('SearchPage')}
+          >
+            <SearchIcon />
+          </Pressable>
+        );
+      },
+    });
+  }, [navigation]);
+
   return (
     <SafeAreaView style={globalStyles.screen}>
       <ScrollView style={globalStyles.mainContainer} showsVerticalScrollIndicator={false}>
